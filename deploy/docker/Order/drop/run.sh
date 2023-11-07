5049a589a57734d458080e9a59684bd8041d9999
PAULA ROMERO SERRANO <Paula.Romero3@alu.uclm.es>
2023-11-07 18:05:13 +0100

    0711 1805

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#"mongo" is the name of the container running mongodb, indicado tambien en el archivo application.properties
+
 while ! curl http://mongo:27017/
 do
   echo "$(date) - still trying"
@@ -7,4 +9,4 @@ do
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location-application.properties
